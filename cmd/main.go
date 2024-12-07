7de4c973faff4cfac8a66619188fb46dd4308b12
Cyril Heiniger <cyrilheiniger@macbook-air-von-cyril.home>
2024-12-07 11:22:08 +0100

    updated handling and flow of the app, still needs work but mostly works fine

--- cmd/main.go
+++ cmd/main.go
@@ -9,11 +9,12 @@ import (
 
 func main() {
 	http.HandleFunc("/auth/register", handlers.RegisterUserHandler)
-	http.HandleFunc("/auth/token", handlers.GenerateTokenHandler)
 	http.HandleFunc("/auth/validate", handlers.ValidateTokenHandler)
 	http.HandleFunc("/auth/login", handlers.LoginHandler)
+	// http.HandleFunc("/list-users", handlers.ListUsersHandler)
 
-	http.Handle("/auth/profile", middleware.TokenAuthMiddleware(http.HandlerFunc(handlers.ProfileHandler)))
+	http.Handle("/auth/token/profile", middleware.TokenAuthMiddleware(http.HandlerFunc(handlers.ProfileHandler)))
+	//http.Handle("/auth/basicauth/profile", handlers.ProfileHandler)
 
 	log.Println("Authentication service running on :8080")
 	log.Fatal(http.ListenAndServe(":8080", nil))
